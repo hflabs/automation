be1dffc3e286e60b6f1a7b9cfece31f438e06b4a
Vasilev Ilya <ilyavas@hflabs.ru>
2024-09-30 21:55:10 +0300

    update story points type to float from int

--- jira/entity.go
+++ jira/entity.go
@@ -29,7 +29,7 @@ type ChangelogItem struct {
 
 type FieldsIssue struct {
 	BusinessValue int          `json:"customfield_10084,omitempty"`
-	StoryPoints   int          `json:"customfield_10083,omitempty"`
+	StoryPoints   float64      `json:"customfield_10083,omitempty"`
 	WeightedJob   float64      `json:"customfield_12580,omitempty"`
 	Status        IssueField   `json:"status,omitempty"`
 	Issuetype     IssueIdField `json:"issuetype,omitempty"`
