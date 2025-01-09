21f6f773f7a669a3f7f193505293643b043cf06a
Vasilev Ilya <ilyavas@hflabs.ru>
2025-01-09 13:02:44 +0300

    healthcheck init

---
+++ healthcheck/go.mod
@@ -0,0 +1,7 @@
+module github.com/hflabs/automation/healthcheck
+
+go 1.23.1
+
+require github.com/carlmjohnson/requests v0.24.3
+
+require golang.org/x/net v0.27.0 // indirect
---
+++ healthcheck/health.go
@@ -0,0 +1,30 @@
+package healthcheck
+
+import (
+	"context"
+	"fmt"
+	"github.com/carlmjohnson/requests"
+	"os"
+	"time"
+)
+
+var url = os.Getenv("MONITORING_URL")
+var serviceName = os.Getenv("SERVICE_NAME")
+
+type Healthcheck struct {
+	ServiceName string `json:"service_name"`
+	Status      int    `json:"status"`
+}
+
+func HealthcheckSender() {
+	for {
+		_ = requests.
+			URL(fmt.Sprintf("%s/%s", url, "healthcheck")).Post().
+			BodyJSON(Healthcheck{
+				ServiceName: serviceName,
+				Status:      1,
+			}).
+			Fetch(context.Background())
+		time.Sleep(time.Minute)
+	}
+}
